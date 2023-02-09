8f0c284f5870e64b0c5ffee3423bca5205f9ea02
lspedalieri <leopoldo.spedalieri@gmail.com>
2023-02-09 23:49:29 +0100

    accorpato script del titolo allo script della scena

--- scripts/StartScreen.gd
+++ scripts/StartScreen.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
 var selection := "play"
+onready var title_animation = get_node("TitleStartingAnimation")
+onready var title = get_node("Title")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Cursor.position.y = 193
 	selection = "play"
+	title_animation.play("start title")
+	move_to_coordinates("Title", title.position.x, 165, 2.0)
+	#move_to_coordinates(x, 125, 0.5)
 
+func move_to_coordinates(node_name, x, y, t) -> void:
+	var tween := get_node(node_name).create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	tween.tween_property(get_node(node_name), "global_position", Vector2(x,y), t)
+	yield(tween, "finished")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
